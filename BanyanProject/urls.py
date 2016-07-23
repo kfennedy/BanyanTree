d9719065014eac4e1aa926e63f7a59c5a1f6ea2f
kfennedy <kfennedy@users.noreply.github.com>
2016-07-23 14:26:49 +0800

    styling all the template layouts for registration

--- BanyanProject/urls.py
+++ BanyanProject/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+
 urlpatterns = [
 url(r'^admin/', admin.site.urls),
 url('', include("BanyanApp.urls")),
+url(r'^accounts/', include('registration.backends.hmac.urls')),
 # url(r'^djangoTutorialApp/$', "<appname>.views.<function_name>"),
 ]
 
